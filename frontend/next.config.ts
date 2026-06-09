9b725191488e3b813b5b1756546bda2a8efdf360
weirdo <nimes.gsdcgecp@gmail.com>
2026-06-09 11:18:02 +0000

    Fix config conflict: apply Vercel routing to next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-output: 'standalone',
+output: process.env.VERCEL === '1' ? undefined: 'standalone',
 };
 
 export default nextConfig;
